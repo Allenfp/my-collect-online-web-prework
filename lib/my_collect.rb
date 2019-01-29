67cc8293e2491898fcc44e8a5cdebd28a0d0fa28
Allen F Partlow <allenfp@gmail.com>
2019-01-29 21:19:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,9 +5,10 @@ def my_collect(collection)
 
   while i < collection.length
   
-    i += 1
-    new_array << yield(collection[i])
 
+    new_array << yield(collection[i])
+    i += 1
+    
   end
 
   new_array
